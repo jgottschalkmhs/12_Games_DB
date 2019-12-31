dbc00456ebc2763addfef51c78816ec83f5478b2
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2019-12-31 16:20:22 +1300

    Genre Dropdown
    Includes working genre dropdown

--- bottombit.php
+++ bottombit.php
@@ -29,9 +29,39 @@
             
             <form class="searchform" method="post" action="advanced.php" enctype="multipart/form-data">
                 
-                <p>Form goes here...</p>
+            <input class="adv" type="text" name="app_name" size="40" value=""  placeholder="App Nam / Title..."/>
+                            
+            <input class="adv" type="text" name="dev_name" size="40" value=""  placeholder="Developer..."/>
                 
-            <input class="submit advanced-button" type="submit" name="advanced" value="Search &nbsp; &#xf002;" />
+
+                
+            <!-- Genre Dropdown -->
+                
+            <select class="search adv" name="genre">
+                
+            <option value="" disabled selected>Genre...</option>
+                
+            <!--- get options from database -->
+            <?php 
+                $genre_sql="SELECT * FROM `genre` ORDER BY `genre`.`Genre` ASC ";
+                $genre_query=mysqli_query($dbconnect, $genre_sql);
+                $genre_rs=mysqli_fetch_assoc($genre_query);
+                
+                do {
+                    ?>
+                <option value="<?php echo $genre_rs['Genre']; ?>"><?php echo $genre_rs['Genre']; ?></option>
+                
+                <?php
+                    
+                }   // end genre do loop
+                
+                while ($genre_rs=mysqli_fetch_assoc($genre_query))
+                
+            ?>
+            
+            </select>
+                
+            <input class="submit advanced-button" type="submit" name="advanced" value="Search &nbsp; &#xf002;" /> 
                 
             </form>
                 
