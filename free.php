73e0ecc746877dc7f9ebbd1706bd93478d0b7082
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2019-12-31 15:20:35 +1300

    Basic Search
    Includes search for name / developer and apps that are free with no in app purchase

--- free.php
+++ free.php
@@ -3,7 +3,7 @@
     $find_sql = "SELECT * FROM `game_details`
     JOIN genre ON (game_details.GenreID = genre.GenreID)
     JOIN developer ON (game_details.DeveloperID = developer.DeveloperID)
-    
+    WHERE `Price` = 0 AND `In App` = 0
     ";
     $find_query = mysqli_query($dbconnect, $find_sql);
     $find_rs = mysqli_fetch_assoc($find_query);
@@ -12,7 +12,7 @@
 ?>
 
         <div class="box main">
-            <h2>All Results</h2>
+            <h2>Free with No In App Purchase</h2>
             
             <?php 
             include ("results.php");
--- index.php
+++ index.php
@@ -5,9 +5,17 @@
             
             
             <p>
-                This database includes some of the most popular apps available for mobile devices.  The <a href="https://www.kaggle.com/tristan581/17k-apple-app-store-strategy-games/version/1">original dataset</a> can be found at the preceding link.
+                This database includes some of the most popular apps available for mobile devices.  
             </p>
             
+            <p>&nbsp;</p>
+            
+            <h3>Acknowledgements &amp; Sources</h3>
+            
+            <ul>
+                <li><a href="https://www.kaggle.com/tristan581/17k-apple-app-store-strategy-games/version/1">original "17K Mobile Strategy Games" dataset</a></li>
+                <li>The <a href="https://codepen.io/Bluetidepro/pen/GkpEa ">percentage based star rating </a> code is at the preceding link.  It was created by user Zach Reed.</li>
+            </ul>
 
             
         </div> <!-- / main -->
